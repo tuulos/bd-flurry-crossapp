088f9f708c73dd430fdec85b8dee00a1ae828eb6
Ville Tuulos <ville@bitdeli.com>
2013-02-01 05:24:27 +0000

    session activity

--- __main__.py
+++ __main__.py
@@ -4,31 +4,28 @@ from bitdeli.widgets import Widget, Text, Title, Description, set_theme
 from itertools import imap, chain
 from datetime import datetime 
 
-set_theme('purple')
+set_theme('eighties')
 
-DAYS = 100
+MINUTES = 60
 
 class Matrix(Widget):
     defaults = {'size': [3,3]}
 
-def day(session):
-    return datetime.utcfromtimestamp(session['t'] / 1000.).toordinal()
-
 data = []
 for profile in profiles():
-    days = frozenset(imap(day, profile['sessions']))
-    first = min(days)
-    row = [0] * DAYS
-    for i in range(DAYS):
-        if i + first in days:
-            row[i] = 1
-        data.append(row)
+    row = [0] * MINUTES
+    for session in profile['sessions']:
+        for event in session['l']:
+           col = min(MINUTES - 1, event['o'] / 60000)
+           row[col] = min(row[col] + 0.1, 1.0)
+    data.append(row)
 
 data.sort(key=lambda r: sum(r), reverse=True)
 data = data[:3000]
 
-Text(size=(2,2),
-     data={'head': len(data)})
+Text(size=(12,2),
+     data={'head': "Session activity over the first 60 minutes",
+           'text': "%d most active users" % len(data)})
 
 Matrix(size=[12,12],
        color=2,
